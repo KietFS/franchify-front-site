072f4e8454c10078f8b83700ef8a9aea26439186
KietFS <kietmakietna@gmail.com>
2024-11-12 01:07:22 +0700

    Fix error server fulfillment management

--- src/components/organisms/FulfillmentMangement/index.tsx
+++ src/components/organisms/FulfillmentMangement/index.tsx
@@ -70,129 +70,140 @@ const FulfillmentMangement: React.FC<IFulfillmentMangementProps> = (props) => {
 
   return (
     <>
-      <button
-        onClick={handleClick}
-        className="p-2 text-black-500 rounded-full w-fit desktop:max-w-[250px] bg-transparent justify-center items-center flex gap-x-2"
-      >
-        <TruckIcon className="w-8 h-8 text-secondary-500 mr-1" />
-        <div>
-          <p className="text-secondary-500 font-regular hidden desktop:flex truncate">
-            Bạn đang chọn
-          </p>
-          <p className="text-secondary-500 font-regular hidden desktop:flex truncate max-w-[100px]">
-            {`Cửa hàng: ${currentStore?.name}`}
-          </p>
-        </div>
-      </button>
-
-      <Popover
-        open={openPopup}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        transformOrigin={{ vertical: -14, horizontal: "center" }}
-        sx={{ width: "200px", height: "500px" }}
-        slotProps={{
-          paper: {
-            sx: {
-              borderRadius: "var(--border-radius-large, 16px)",
-              overflow: "visible",
-              maxWidth: 327,
-              boxShadow: "0px 8px 28px 0px rgba(0, 0, 0, 0.28)",
-              "&:before": {
-                content: '""',
-                display: "block",
-                position: "absolute",
-                top: 0,
-                right: "50%",
-                left: "50%",
-                width: 20,
-                height: 20,
-                backgroundColor: "rgb(249 250 251)",
-                transform: "translateY(-50%) rotate(45deg)",
-                boxShadow: "-3px -3px 5px -2px rgba(0,0,0,0.1)",
-              },
-            },
-          },
-        }}
-      >
-        <div className="w-[280px] border border-secondary-100 min-h-[400px] shadow-sm bg-secondary-50 px-4 py-8 rounded-lg flex flex-col gap-y-4">
-          <div>
-            <p className="text-secondary-900 text-sm font-normal">
-              Bạn đang mua sắm ở
-            </p>
-            <strong className="text-primary-600 text-lg">
-              {currentStore?.name}
-            </strong>
-          </div>
-
-          <Divider sx={{ height: 4, width: "100%", margin: "4px 0" }} />
-
-          <div>
-            <div className="flex items-center gap-x-2">
-              {isStoreOpen(currentStore?.openTime, currentStore?.closeTime) ? (
-                <>
-                  <BuildingStorefrontIcon className="w-4 h-4 text-green-500" />
-                  <p className="text-sm text-green-600 font-bold">
-                    Đang mở cửa
-                  </p>
-                </>
-              ) : (
-                <>
-                  <BuildingStorefrontIcon className="w-4 h-4 text-red-500" />
-                  <p className="text-sm text-red-600 font-bold">Đã đóng cửa</p>
-                </>
-              )}
-            </div>
-            <strong className="text-secondary-900 font-normal text-sm">
-              5201 4th St #7, Lubbock, TX 79416
-            </strong>
-          </div>
-
-          <a className="no-underline text-secondary-900 text-sm font-semibold cursor-pointer hover:underline">
-            Store Detail
-          </a>
-
-          <Divider sx={{ height: 4, width: "100%", margin: "4px 0" }} />
-
-          <div>
-            <div className="flex items-center gap-x-2">
-              <ShoppingBagIcon className="w-8 h-8 text-primary-500" />
-              <div>
-                <p className="text-secondary-800 text-sm font-semibold">
-                  Free for orders over $30{" "}
-                </p>
-                <p className="text-sm text-secondary-800 font-normal">
-                  Varies by location
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <Button
-            onClick={() => {
-              setOpenPopup(false);
-              setOpenSelectStore(true);
-            }}
-            className="mt-4"
+      {!!currentStore ? (
+        <>
+          <button
+            onClick={handleClick}
+            className="p-2 text-black-500 rounded-full w-fit desktop:max-w-[250px] bg-transparent justify-center items-center flex gap-x-2"
           >
-            Danh sách cửa hàng
-          </Button>
-        </div>
-      </Popover>
+            <TruckIcon className="w-8 h-8 text-secondary-500 mr-1" />
+            <div>
+              <p className="text-secondary-500 font-regular hidden desktop:flex truncate">
+                Bạn đang chọn
+              </p>
+              <p className="text-secondary-500 font-regular hidden desktop:flex truncate max-w-[100px]">
+                {`Cửa hàng: ${currentStore?.name}`}
+              </p>
+            </div>
+          </button>
 
-      {openSelectStore ? (
-        <CustomDialog
-          maxWidth="sm"
-          title="Chọn cửa hàng bạn muốn mua hàng"
-          children={
-            <>
-              <StoreList onSelectedStore={() => setOpenSelectStore(false)} />
-            </>
-          }
-          open={openSelectStore}
-          onClose={() => setOpenSelectStore(false)}
-        />
+          <Popover
+            open={openPopup}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            transformOrigin={{ vertical: -14, horizontal: "center" }}
+            sx={{ width: "200px", height: "500px" }}
+            slotProps={{
+              paper: {
+                sx: {
+                  borderRadius: "var(--border-radius-large, 16px)",
+                  overflow: "visible",
+                  maxWidth: 327,
+                  boxShadow: "0px 8px 28px 0px rgba(0, 0, 0, 0.28)",
+                  "&:before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: "50%",
+                    left: "50%",
+                    width: 20,
+                    height: 20,
+                    backgroundColor: "rgb(249 250 251)",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    boxShadow: "-3px -3px 5px -2px rgba(0,0,0,0.1)",
+                  },
+                },
+              },
+            }}
+          >
+            <div className="w-[280px] border border-secondary-100 min-h-[400px] shadow-sm bg-secondary-50 px-4 py-8 rounded-lg flex flex-col gap-y-4">
+              <div>
+                <p className="text-secondary-900 text-sm font-normal">
+                  Bạn đang mua sắm ở
+                </p>
+                <strong className="text-primary-600 text-lg">
+                  {currentStore?.name}
+                </strong>
+              </div>
+
+              <Divider sx={{ height: 4, width: "100%", margin: "4px 0" }} />
+
+              <div>
+                <div className="flex items-center gap-x-2">
+                  {isStoreOpen(
+                    currentStore?.openTime,
+                    currentStore?.closeTime,
+                  ) ? (
+                    <>
+                      <BuildingStorefrontIcon className="w-4 h-4 text-green-500" />
+                      <p className="text-sm text-green-600 font-bold">
+                        Đang mở cửa
+                      </p>
+                    </>
+                  ) : (
+                    <>
+                      <BuildingStorefrontIcon className="w-4 h-4 text-red-500" />
+                      <p className="text-sm text-red-600 font-bold">
+                        Đã đóng cửa
+                      </p>
+                    </>
+                  )}
+                </div>
+                <strong className="text-secondary-900 font-normal text-sm">
+                  5201 4th St #7, Lubbock, TX 79416
+                </strong>
+              </div>
+
+              <a className="no-underline text-secondary-900 text-sm font-semibold cursor-pointer hover:underline">
+                Store Detail
+              </a>
+
+              <Divider sx={{ height: 4, width: "100%", margin: "4px 0" }} />
+
+              <div>
+                <div className="flex items-center gap-x-2">
+                  <ShoppingBagIcon className="w-8 h-8 text-primary-500" />
+                  <div>
+                    <p className="text-secondary-800 text-sm font-semibold">
+                      Free for orders over $30{" "}
+                    </p>
+                    <p className="text-sm text-secondary-800 font-normal">
+                      Varies by location
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <Button
+                onClick={() => {
+                  setOpenPopup(false);
+                  setOpenSelectStore(true);
+                }}
+                className="mt-4"
+              >
+                Danh sách cửa hàng
+              </Button>
+            </div>
+          </Popover>
+
+          {openSelectStore ? (
+            <CustomDialog
+              maxWidth="sm"
+              title="Chọn cửa hàng bạn muốn mua hàng"
+              children={
+                <>
+                  <StoreList
+                    onSelectedStore={() => setOpenSelectStore(false)}
+                  />
+                </>
+              }
+              open={openSelectStore}
+              onClose={() => setOpenSelectStore(false)}
+            />
+          ) : null}
+        </>
       ) : null}
     </>
   );
