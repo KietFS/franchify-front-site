4971038bf8b170347d6fefc815d5f0c0e8834d72
KietFS <kietmakietna@gmail.com>
2024-12-26 23:56:19 +0700

    Update github workflow

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,11 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('homepage has title and links to about page', async ({ page }) => {
+test("homepage has title and links to about page", async ({ page }) => {
   // Go to the homepage
-   await page.goto('http://localhost:3000/');
-
-    // Check that the title is correct
-    // const title = page.locator('title');
-    // await expect(title).toHaveText('tea-market.vercel.app');
-
+  await page.goto("http://localhost:3000/");
 });
